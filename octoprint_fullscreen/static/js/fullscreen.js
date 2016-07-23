9c84ee71d3c75fdf56860ba7e1d0f8afdce8ca35
BillyBlaze <pablo@blacktower.nl>
2016-07-23 10:59:59 +0000

    :memo: Whitespace commit (WOOHOOOgit push)

--- octoprint_fullscreen/static/js/fullscreen.js
+++ octoprint_fullscreen/static/js/fullscreen.js
@@ -6,16 +6,16 @@
  * License: AGPLv3
  */
 $(function() {
-    function FullscreenViewModel(parameters) {
-        var self = this;
+	function FullscreenViewModel(parameters) {
+		var self = this;
 		var $webcam = $('#webcam_image');
 		var $container = $('#webcam_rotator');
 		var $info = $('#fullscreen-bar');
 		var $body = $('body');
 		
-        self.tempModel = parameters[0];
+		self.tempModel = parameters[0];
 		self.printer = parameters[2];
-        self.settings = parameters[1];
+		self.settings = parameters[1];
 		
 		self.printer.isFullscreen = ko.observable(false);
 		self.printer.fullscreen = function() {
@@ -43,22 +43,22 @@ $(function() {
 		$("#job_pause").clone().appendTo(".user-buttons");
 		
 		ko.applyBindings(self.printer, document.getElementById("fullscreen-cancel"))
-    }
+	}
 
-    OCTOPRINT_VIEWMODELS.push([
-        FullscreenViewModel,
-        ["temperatureViewModel", "settingsViewModel", "printerStateViewModel"],
-        ["#fullscreen-info"]
-    ]);
+	OCTOPRINT_VIEWMODELS.push([
+		FullscreenViewModel,
+		["temperatureViewModel", "settingsViewModel", "printerStateViewModel"],
+		["#fullscreen-info"]
+	]);
 });
 
 function formatBarTemperature(toolName, actual, target) {
-    var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
+	var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
 
-    if (target) {
-        var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
-        output += sign + _.sprintf("%.1f&deg;C", target);
-    }
+	if (target) {
+		var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
+		output += sign + _.sprintf("%.1f&deg;C", target);
+	}
 
-    return output;
+	return output;
 };
