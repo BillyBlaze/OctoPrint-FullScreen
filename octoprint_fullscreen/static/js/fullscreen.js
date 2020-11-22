18e9084f96f3d81efb7e97fb2e53201cefaf1ffe
BillyBlaze <BillyBlaze@users.noreply.github.com>
2020-11-22 14:29:11 +0000

    :art: Fix for OctoPrint 1.5.0 RC
    Fixes #32

--- octoprint_fullscreen/static/js/fullscreen.js
+++ octoprint_fullscreen/static/js/fullscreen.js
@@ -14,19 +14,8 @@ if (window.location.hash.indexOf('-fullscreen-open') !== -1) {
 $(function() {
 	function FullscreenViewModel(parameters) {
 		var self = this;
-		var $webcam = $('#webcam_image');
-		var $info = $('#fullscreen-bar');
 		var $body = $('body');
 		var $container, $fullscreenContainer;
-
-		var containerPlaceholder = document.getElementById('webcam') ? '#webcam' : '#webcam_container';
-		if ($('.webcam_fixed_ratio').length > 0) {
-			$container = $(containerPlaceholder + ' .webcam_fixed_ratio');
-			$fullscreenContainer = $(containerPlaceholder + ' #webcam_rotator');
-		} else {
-			$container = $(containerPlaceholder + ' #webcam_rotator');
-			$fullscreenContainer = $(containerPlaceholder + ' #webcam_container');
-		}
 
 		self.tempModel = parameters[0];
 		self.printer = parameters[1];
@@ -41,6 +30,45 @@ $(function() {
 		}
 
 		self.onStartupComplete = function() {
+			var $webcam = $('#webcam_image');
+			var $info = $('#fullscreen-bar');
+	
+			var containerPlaceholder = document.getElementById('webcam') ? '#webcam' : '#webcam_container';
+			if ($('.webcam_fixed_ratio').length > 0) {
+				$container = $(containerPlaceholder + ' .webcam_fixed_ratio');
+				$fullscreenContainer = $(containerPlaceholder + ' #webcam_rotator');
+			} else {
+				$container = $(containerPlaceholder + ' #webcam_rotator');
+				$fullscreenContainer = $(containerPlaceholder + ' #webcam_container');
+			}
+
+			var touchtime = 0;
+			$webcam.on("click", function() {
+				if (touchtime === 0) {
+					touchtime = new Date().getTime();
+				} else {
+					if (((new Date().getTime()) - touchtime) < 800) {
+						$body.toggleClass('inlineFullscreen');
+						$container.toggleClass("inline fullscreen");
+	
+						if ($body.hasClass('inlineFullscreen')) {
+							history.pushState('', null, window.location.hash + '-fullscreen-open');
+						} else {
+							if (window.location.hash.indexOf('-fullscreen-open') !== -1) {
+								history.pushState('', null, window.location.hash.replace('-fullscreen-open', ''));
+							}
+						}
+	
+						if(self.printer.fsp.isFullscreen()) {
+							$fullscreenContainer.toggleFullScreen();
+						}
+						touchtime = 0;
+					} else {
+						touchtime = new Date().getTime();
+					}
+				}
+			});
+
 			if (onceOpenInlineFullscreen) {
 				setTimeout(function() {
 					touchtime = new Date().getTime();
@@ -48,6 +76,13 @@ $(function() {
 					onceOpenInlineFullscreen = false;
 				}, 100);
 			}
+
+			$info.insertAfter($container);
+			$(".print-control #job_pause").clone().appendTo("#fullscreen-bar .user-buttons").attr('id', 'job_pause_clone');
+	
+			ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-print-info").get(0));
+			ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-buttons").get(0));
+			ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-progress-bar").get(0));
 		}
 
 		self.onDataUpdaterPluginMessage = function (plugin, data) {
@@ -73,48 +108,14 @@ $(function() {
 			return output;
 		};
 
-		var touchtime = 0;
-		$webcam.on("click", function() {
-			if (touchtime === 0) {
-				touchtime = new Date().getTime();
-			} else {
-				if (((new Date().getTime()) - touchtime) < 800) {
-					$body.toggleClass('inlineFullscreen');
-					$container.toggleClass("inline fullscreen");
-
-					if ($body.hasClass('inlineFullscreen')) {
-						history.pushState('', null, window.location.hash + '-fullscreen-open');
-					} else {
-						if (window.location.hash.indexOf('-fullscreen-open') !== -1) {
-							history.pushState('', null, window.location.hash.replace('-fullscreen-open', ''));
-						}
-					}
-
-					if(self.printer.fsp.isFullscreen()) {
-						$fullscreenContainer.toggleFullScreen();
-					}
-					touchtime = 0;
-				} else {
-					touchtime = new Date().getTime();
-				}
-			}
-		});
-
 		$(document).bind("fullscreenchange", function() {
 			self.printer.fsp.isFullscreen($(document).fullScreen());
 		});
-
-		$info.insertAfter($container);
-		$(".print-control #job_pause").clone().appendTo("#fullscreen-bar .user-buttons").attr('id', 'job_pause_clone');
-
-		ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-print-info").get(0));
-		ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-buttons").get(0));
-		ko.applyBindings(self.printer, $("#fullscreen-bar #fullscreen-progress-bar").get(0));
 	}
 
 	OCTOPRINT_VIEWMODELS.push({
 		construct: FullscreenViewModel,
-		dependencies: ["temperatureViewModel", "printerStateViewModel"],
+		dependencies: ["temperatureViewModel", "printerStateViewModel", "controlViewModel"],
 		optional: [],
 		elements: ["#fullscreen-tool-info"]
 	});
